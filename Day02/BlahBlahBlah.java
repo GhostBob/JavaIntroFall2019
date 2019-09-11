e04728874f32fd1e03e63b25e5e4c4e8e05e404a
Schroeder <SchroederTheCoder@gmail.com>
2019-09-11 16:43:42 -0500

    Day 2.

---
+++ Day02/BlahBlahBlah.java
@@ -0,0 +1,7 @@
+package blahblahblah;
+
+public class BlahBlahBlah {
+	public static void main( String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
---
+++ Day02/JavaApplication65.java
@@ -0,0 +1,74 @@
+package javaapplication65;
+
+import java.util.Date;
+
+/**
+ * A JavaApplication, 65th in production. Makes people happy.
+ * Not really, haha.
+ * 
+ * @author Schroeder <schroeder.austincc.edu>
+ */
+public class JavaApplication65 {
+
+    /**
+     * This is main. It sucks.
+     * @param args The args, like any program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        //TOP FOUR!
+        int inty = 3; //Integer, a whole number.
+        double d = 3.1415; //Double, a real number.
+        boolean b = true; //Boolean, holds either a true or a false.
+        char c = 'a'; //Characters, holds a single letter. Not necessarily alphabet.
+
+        //Yeah, ok...
+        float f = 3.1415f; //Float, like a double, only it sucks. Unless doing CG.
+        short s = 3; //Short, a whole number, but very small.
+        long l = 3; //Long, a whole number, but very long.
+        byte yep = 3; //Byte, the shortest of the whole numbers.
+
+        // Stores a "string" of characters.
+//        String hello = "world!";
+//        String age = "123";
+//        System.out.println(age + 1); //Be careful, this produces "1231"
+        int x = 0, y = 0, z = 0;
+//        System.out.println(x);
+
+        int ohmygoditscomingrightforus = 123;
+        int oh_my_god_its_coming_right_for_us; // Python developer.
+        int wePreferCamelCaseSkippingCapitalizingTheFirstLetter = 3;
+        String sourceUrl = "http://dgknag";
+        int $welcomeHOme;
+        int __accidentallyAwesome2342;
+        int X = 3;
+
+//        System.out.println(x);
+//        System.out.println(X);
+        int isTrue;
+        boolean is_true;
+        int Int = 3;
+
+        //Constants, which are variables that will never change.
+        final double PI = 3.1415;
+        final int NUMBER_OF_PLANETS = 8;
+
+        System.out.println("The value of x: " + x);
+        System.out.println("Just the letter x: " + "x");
+
+        char ha = 8;
+        System.out.println("Hello, world!" + ha + ha + ha);
+        
+        System.out.print("This is ");
+        System.out.println("awesome!");
+
+//        System.out.println("Byte Max: " + Byte.MIN_VALUE);
+//        System.out.println("Short Max: " + Short.MIN_VALUE);
+//        System.out.println("Int Max: " + Integer.MIN_VALUE);
+//        System.out.println("Long Max: " + Long.MIN_VALUE);
+//        
+//        System.out.println("Double Max: " + Double.MAX_VALUE);
+    }
+
+}
